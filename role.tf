c8c7ac9d18e5396ae05dd83a8451897c49fb449b
Ash Davies <3853061+DrizzlyOwl@users.noreply.github.com>
2024-04-18 17:48:49 +0100

    Fixed invalid role name

--- role.tf
+++ role.tf
@@ -2,7 +2,7 @@ resource "azurerm_role_assignment" "key_user_role" {
   count = local.assign_rbac_role && local.container_identity_principal_id != "" ? 1 : 0
 
   scope                = local.key_vault.id
-  role_definition_name = "Key Vault Crypto Secret Encryption User"
+  role_definition_name = "Key Vault Crypto Service Encryption User"
   principal_id         = local.container_identity_principal_id
   description          = "Allow Azure Container Apps to use the Keys defined inside Key Vault"
 }

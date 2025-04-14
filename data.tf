db0ccbd19779baa0284e2d459260beb81f345301
Ash Davies <3853061+DrizzlyOwl@users.noreply.github.com>
2025-04-14 14:53:45 +0100

    Use 'time' resource for state-managed expiry dates (#22)
    Ignore changes of expiration date
    
    * We have an automatic rotation policy that will change the expiration date when the key rotates, so we probably dont want terraform to rebuild the entire key again

--- data.tf
+++ data.tf
@@ -11,3 +11,10 @@ data "azurerm_virtual_network" "vnet" {
   name                = local.vnet_name
   resource_group_name = local.resource_group_name
 }
+
+resource "time_static" "key_expiration_date" {
+  triggers = {
+    # Save the time when Key Vault is built
+    key_vault_id = local.key_vault.id
+  }
+}
--- locals.tf
+++ locals.tf
@@ -6,12 +6,11 @@ locals {
   container_identity_principal_id = var.data_protection_container_identity_principal_id
   subnet_prefix                   = var.data_protection_key_vault_subnet_prefix
   key_vault_access_ipv4           = var.data_protection_key_vault_access_ipv4
+  key_expiration_date             = timeadd(time_static.key_expiration_date.id, "${local.key_expiry_years * 8760}h")
   resource_group_name             = var.data_protection_resource_group_name
   vnet_name                       = var.data_protection_vnet_name == "" ? "${local.resource_prefix}default" : var.data_protection_vnet_name
   azure_location                  = var.data_protection_azure_location
   key_expiry_years                = var.data_protection_key_expiry_years
-  timestamp_parts                 = regex("^(?P<year>\\d+)(?P<remainder>-.*)$", timestamp())
-  year_from_now                   = format("%d%s", local.timestamp_parts.year + local.key_expiry_years, local.timestamp_parts.remainder)
   tags                            = var.data_protection_tags
   enable_diagnostic_setting       = var.data_protection_enable_diagnostic_setting
   enable_log_analytics_workspace  = var.data_protection_diagnostic_log_analytics_workspace_id == "" ? var.data_protection_enable_log_analytics_workspace : false
